3f78825c71805fb09d564dc7e20995f0e5c0521c
rudhyrudoolf <37206688+rudhyrudoolf@users.noreply.github.com>
2021-10-04 16:39:57 +0700

    Update say_hello.go
    add function address

--- say_hello.go
+++ say_hello.go
@@ -3,3 +3,7 @@ package gosayhello
 func SayHello(firstName string, lastName) string {
 	return "Hello " + name + " " + lastName
 }
+
+func Addrees(firstname string, lastName string , Address string) string {
+	return sayHello(firstname,lastName) + "Address"
+}
