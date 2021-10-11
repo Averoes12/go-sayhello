866b076aff8421d9f0edc96afd9deead46726b25
rudhyrudolf <rudhyhermawan97@gmail.com>
2021-10-11 09:15:34 +0700

    resolve bugand move folder

---
+++ myfunction/say_hello.go
@@ -0,0 +1,9 @@
+package myfunction
+
+func SayHello(firstName string, lastName string) string {
+	return "Hello " + firstName + " " + lastName
+}
+
+func Addrees(Address string) string {
+	return "My Address : " + Address
+}
